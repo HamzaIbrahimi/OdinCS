617d3edd857837aeff382d92bbf47580734e5297
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-06-02 04:51:25 +0200

    Added removeAt and insertAt methods for the linkedList

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -21,7 +21,7 @@ export default class LinkedList {
   }
 
   append(value) {
-    let oldTail = this.#tail;
+    const oldTail = this.#tail;
     this.#tail = new Node();
     this.#tail.value = value;
     if (this.isEmpty()) {
@@ -33,7 +33,7 @@ export default class LinkedList {
   }
 
   prepend(value) {
-    let oldHead = this.#head;
+    const oldHead = this.#head;
     this.#head = new Node();
     this.#head.value = value;
     if (this.isEmpty()) {
@@ -79,7 +79,7 @@ export default class LinkedList {
       throw new Error("Can't pop an empty LinkedList");
     }
     if (this.#head === this.#tail) {
-      let removed = this.#head.value;
+      const removed = this.#head.value;
       this.#head = null;
       this.#tail = null;
       this.#size--;
@@ -124,6 +124,53 @@ export default class LinkedList {
       temp = temp.nextNode;
     }
     return null;
+  }
+
+  insertAt(value, index) {
+    if (index < 0 || index > this.#size) {
+      throw new RangeError(`Index out of bounds for insertAt(${index})`);
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    } else if (index === this.#size) {
+      this.append(value);
+      return;
+    }
+    const currentNode = this.at(index);
+    const beforeCurrentNode = this.at(index - 1);
+    const newNode = new Node();
+    newNode.value = value;
+    newNode.nextNode = currentNode;
+    beforeCurrentNode.nextNode = newNode;
+    this.#size++;
+    return;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index > this.#size - 1) {
+      throw new Error(
+        `Index out of bounds for linkedList of size: ${this.#size}`
+      );
+    }
+    if (this.isEmpty()) {
+      throw new Error('Cannot remove from an empty LinkedList');
+    }
+    if (index === this.#size - 1) {
+      this.pop();
+      return;
+    }
+    if (index === 0) {
+      this.#head = this.#head.nextNode;
+      this.#size--;
+      return;
+    }
+    const current = this.at(index);
+    const beforeCurrent = this.at(index - 1);
+    beforeCurrent.nextNode = current.nextNode;
+    current.nextNode = null;
+    this.#size--;
+    return;
   }
 
   toString() {
