18686c5a3cb7cbd1e90867d362435504cbee8505
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-08-13 01:04:44 +0200

    Some progress but some issues present

--- KnightsTravails/knighttravails.js
+++ KnightsTravails/knighttravails.js
@@ -14,8 +14,6 @@ function possiblePositions(position) {
     return moves;
 }
 
-console.log(possiblePositions([0, 0]));
-
 function exactMoves() {
     let [x, y] = [1, 2];
     let a = [];
@@ -37,3 +35,34 @@ function exactMoves() {
     }
     return a;
 }
+
+function containsPosition(position, array) {
+    for (const pos of array) {
+        if (pos[0] === position[0] && pos[1] === position[1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function knightMoves(s, d) {
+    const visited = [];
+    const q = new Queue();
+    q.enqueue(s);
+    visited.push(s);
+    while (!q.isEmpty()) {
+        let currPosition = q.dequeue();
+        for (let position of possiblePositions(currPosition)) {
+            if (!containsPosition(position, visited)) {
+                q.enqueue(position);
+                visited.push(position);
+                if (containsPosition(d, visited)) {
+                    return visited;
+                }
+            }
+        }
+    }
+    return visited;
+}
+
+console.log(knightMoves([0, 0], [3, 3]));
